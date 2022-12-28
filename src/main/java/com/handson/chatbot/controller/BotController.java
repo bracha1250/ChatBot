2d6d61a3f017385e27f316cf07a38f7690f45f58
root <brachi1770@gmail.com>
2022-12-28 18:14:15 +0200

    commit - with bot response

--- src/main/java/com/handson/chatbot/controller/BotController.java
+++ src/main/java/com/handson/chatbot/controller/BotController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.net.JarURLConnection;
+import java.util.HashMap;
 
 @Service
 @RestController
@@ -28,5 +29,52 @@ JokesEervice jokesEervice;
     @RequestMapping(value = "/jokes", method = RequestMethod.GET)
     public ResponseEntity<?> getJokes(@RequestParam String keyword) throws IOException {
         return new ResponseEntity<>(jokesEervice.getJokesValue(keyword), HttpStatus.OK);
+    }
+    @RequestMapping(value = "", method = { RequestMethod.POST})
+    public ResponseEntity<?> getBotResponse(@RequestBody BotQuery query) throws IOException {
+        HashMap<String, String> params = query.getQueryResult().getParameters();
+        String res = "Not found";
+        if (params.containsKey("subject")) {
+            res = jokesEervice.getJokesValue(params.get("subject"));
+        } else if (params.containsKey("product")) {
+            res = amazonService.searchProducts(params.get("product"));
+        }
+        return new ResponseEntity<>(BotResponse.of(res), HttpStatus.OK);
+    }
+
+
+    static class BotQuery {
+        QueryResult queryResult;
+
+        public QueryResult getQueryResult() {
+            return queryResult;
+        }
+    }
+
+    static class QueryResult {
+        HashMap<String, String> parameters;
+
+        public HashMap<String, String> getParameters() {
+            return parameters;
+        }
+    }
+
+    static class BotResponse {
+        String fulfillmentText;
+        String source = "BOT";
+
+        public String getFulfillmentText() {
+            return fulfillmentText;
+        }
+
+        public String getSource() {
+            return source;
+        }
+
+        public static BotResponse of(String fulfillmentText) {
+            BotResponse res = new BotResponse();
+            res.fulfillmentText = fulfillmentText;
+            return res;
+        }
     }
 }
